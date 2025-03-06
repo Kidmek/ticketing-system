94d27d855dbb4ec50943c2b8251848b867c40c9e
Kidus Mekonnen <Kidusmekonnen591@gmail.com>
2025-03-06 16:26:28 +0300

    frontend api url changed to use env in prod

--- frontend/src/api/axiosInstance.ts
+++ frontend/src/api/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance, InternalAxiosRequestConfig } from "axios";
 import { store } from "../store/store";
 
-const BASE_URL = "http://localhost:9000";
+const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:9000";
 
 const axiosInstance: AxiosInstance = axios.create({
   baseURL: BASE_URL,
